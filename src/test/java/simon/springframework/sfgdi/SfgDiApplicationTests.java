cf161e9f5e60227966473041f4b05722bc3edc84
simon <szymon.stypka@o2.pl>
2020-11-09 22:46:45 +0100

    Spring Initializr

---
+++ src/test/java/simon/springframework/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package simon.springframework.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
