2e369acbaefc63fb4711c08651ded8193b8354d0
simon <szymon.stypka@o2.pl>
2020-11-09 23:08:47 +0100

    The Spring Context

---
+++ src/main/java/simon/springframework/sfgdi/controllers/MyController.java
@@ -0,0 +1,14 @@
+package simon.springframework.sfgdi.controllers;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello(){
+        System.out.println("Hello World!!!");
+
+        return "Hi Simon";
+    }
+}
